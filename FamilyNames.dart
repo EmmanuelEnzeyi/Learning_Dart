6371f5d1445a3fe1ffbdae4908029479e996021b
EmmanuelEnzeyi <emmanuelsiemaenzeyi@gmail.com>
2024-08-09 13:31:50 +0300

    add FamilyNames file

---
+++ FamilyNames.dart
@@ -0,0 +1,16 @@
+void main() {
+  var dadName = "Eliud Siema";
+  const dadSurname = "Enzeyi";
+  var momName = "Nancy Engesha Singano";
+  var firstBorn = "Emmanuel Siema";
+  var secondBorn = "Faith Buyanzi";
+  var thirdBorn = "Precious Mmbone";
+  var fourthBorn = "Shadrack Fadhili";
+  print('''This is a list og our family members:
+    1. ${dadName} + ${dadSurname}
+    2. ${momName} + ${dadSurname}
+    3. ${firstBorn} + ${dadSurname}
+    4. ${secondBorn} + ${dadSurname}
+    5. ${thirdBorn} + ${dadSurname}
+    6. ${fourthBorn} + ${dadSurname}''');
+}
